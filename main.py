d1b9c999abe1d6f789ee4f2ef5f4c9376ef18d53
VISHWANATH PRATAP SINGH <49968961+vishnupsingh523@users.noreply.github.com>
2020-10-26 21:55:30 +0530

    changes done

--- main.py
+++ main.py
@@ -1,9 +1,154 @@
 from tkinter import *
 import tkinter.font as font
+import time
+import os
 
 root=Tk()
 root.title("Login Page")
-root.geometry("850x400")
+root.geometry("850x500")
+
+
+# ------------------------------------------ LOADING PAGE GIF EDITING---------------------------------------------------
+# gifframe = Frame().pack()
+# frames = [PhotoImage(gifframe,file='images/loading.gif',format = 'gif -index %i' %(i)) for i in range(100)]
+#
+# def update(ind):
+#
+#     frame = frames[ind]
+#     count = 0
+#     if ind == 99:
+#         ind = 0
+#         count = count +1
+#     else:
+#         ind = ind + 3
+#     label.configure(image=frame)
+#     root.after(100, update, ind)
+#     if count == 2:
+#         gifframe.forget()
+#
+#
+# label = Label(root)
+# label.pack()
+# root.after(0, update, 0)
+
+# ---------------------------------------- STUDENT DASHBOARD FUNCTIONS--------------------------------------------------
+# ---------------------------------------- EXAMINATION FUNCTION ------------------------------------------------------------
+def examination():
+        mainframe = Frame(root, bg="#FF5733", bd=2)
+        mainframe.pack(pady=40, padx=40)
+        headingText = Label(mainframe, text="Examination Details", bg="#FF5733", font="Arial 20 bold")
+        headingText.pack()
+        bodyFrame = Frame(mainframe, padx=10, pady=10)
+        bodyFrame.pack()
+
+# examination()
+def showStudentDetails():
+    profileFrame = Frame(bg="white")
+    profileFrame.pack(pady=40)
+    profileHeading = Label(profileFrame, text="Profile Details", bg="#FFDC00", font="Arial 18 bold", pady=10)
+    profileHeading.pack(fill=X)
+    # declaring a new frame for the data
+    dataFrame = Frame(profileFrame)
+    dataFrame.pack(pady=10, padx=10)
+    # ---------------- HERE WE WILL BE FETCHING THE DATA WITH THE HELP OF FILE AND INSERT ALL THE DATA INSIDE THE VARIABLES AND AFTER THAT WE WILL INSERT THAT VARIABLE IN THE STUDENT'S DETAILS.---------------------------------
+    Name = Label(dataFrame, text="Name :", width=20, anchor=E).grid(row=0, column=0)
+    studentName = Label(dataFrame, width=20, anchor=W).grid(row=0, column=1)
+
+    Reg = Label(dataFrame, text="Rregistration Number :", width=20, anchor=E).grid(row=1, column=0)
+    studentReg = Label(dataFrame, width=20, anchor=W).grid(row=1, column=1)
+
+    Course = Label(dataFrame, text="Course :", width=20, anchor=E).grid(row=2, column=0)
+    studentCourse = Label(dataFrame, width=20, anchor=W).grid(row=2, column=1)
+
+    Email = Label(dataFrame, text="Email :", width=20, anchor=E).grid(row=3, column=0)
+    studentEmail = Label(dataFrame, width=20, anchor=W).grid(row=3, column=1)
+
+    Phone = Label(dataFrame, text="Phone :", width=20, anchor=E).grid(row=4, column=0)
+    studentPhone = Label(dataFrame, width=20, anchor=W).grid(row=4, column=1)
+
+    ParentPhone = Label(dataFrame, text="Parent's Phone :", width=20, anchor=E).grid(row=5, column=0)
+    studentParentPhone = Label(dataFrame, width=20, anchor=W).grid(row=5, column=1)
+
+    Address = Label(dataFrame, text="Address :", width=20, anchor=E).grid(row=6, column=0)
+    studentAddress = Label(dataFrame, width=20, anchor=W).grid(row=6, column=1)
+
+
+# -------------------------------------------------contact us--------------
+def contactUs():
+    mainframe = Frame(root, bg="#FF5733", bd=2)
+    mainframe.pack(pady=40, padx=40)
+    headingText = Label(mainframe, text="Contact Us", bg="#FF5733", font="Arial 20 bold")
+    headingText.pack()
+    bodyFrame = Frame(mainframe, padx=10, pady=10)
+    bodyFrame.pack()
+    # Phone numbers of university
+    phone = Label(bodyFrame, text="Phone:", font="Arial 15 bold").grid(row=0, column=0)
+    Label(bodyFrame, text="8928340197", font="Arial 11 bold", width=40, anchor=W).grid(row=1, column=1)
+    Label(bodyFrame, text="8928340197", font="Arial 11 bold", width=40, anchor=W).grid(row=2, column=1)
+    Label(bodyFrame, text="8928340197", font="Arial 11 bold", width=40, anchor=W).grid(row=3, column=1)
+
+    # emails
+    emails = Label(bodyFrame, text="Email:", font="Arial 15 bold").grid(row=4, column=0)
+    email1 = Label(bodyFrame, text="shashwatsingh@gmail.com", font="Arial 11 bold", width=40, anchor=W).grid(row=5,
+                                                                                                             column=1)
+    email2 = Label(bodyFrame, text="vishnupsingh523@gmail.com", font="Arial 11 bold", width=40, anchor=W).grid(row=6,
+                                                                                                               column=1)
+    email3 = Label(bodyFrame, text="adityasinghpratham@gmail.com", font="Arial 11 bold", width=40, anchor=W).grid(row=7,
+                                                                                                                  column=1)
+
+    # websites
+    websites = Label(bodyFrame, text="Websites:", font="Arial 15 bold").grid(row=8, column=0)
+    website1 = Label(bodyFrame, text="https://linkedin.com/shashwatsingh", font="Arial 11 bold", width=40,
+                     anchor=W).grid(row=9, column=1)
+    website2 = Label(bodyFrame, text="https://linkedin.com/vishwanathpratapsingh", font="Arial 11 bold", width=40,
+                     anchor=W).grid(row=10, column=1)
+    website3 = Label(bodyFrame, text="https://linkedin.com/adityasinghpratham", font="Arial 11 bold", width=40,
+                     anchor=W).grid(row=11, column=1)
+
+
+# ----------------------------------------------------STUDENT DASHBOARD ---------------------------------------------------------------
+def studentDashboard():
+
+    root.title(100*" "+"Student Dashboard")
+
+# adding top frame
+    top_frame = Frame(bg = "#001f3f", borderwidth =4 )
+    top_frame.pack(fill = X)
+
+# adding Label to top_frame
+    topText = Label(top_frame,text = "Rajputana University - Student Dashboard",bg = "#001f3f", fg = "white",font="Arial 14 bold")
+    topText.pack(pady = 10)
+
+# -------------------------------------------------------------
+
+
+# -------------------------------------------------------- CREATING BUTTONS ----------------------------------------
+# frame for menuTab
+    menuTab = Frame(bg = "orange",borderwidth = 0)
+    menuTab.pack(fill=Y , side = LEFT)
+# inserting the buttons to the menuTab
+    home = Button(menuTab, text = "Home",cursor = "hand2",borderwidth = 0)
+    home.pack(fill =X,padx=10, pady=20)
+
+    cources = Button(menuTab, text = "Cources", cursor = "hand2",borderwidth = 0)
+    cources.pack(fill =X,padx=10, pady=20)
+
+    examination = Button(menuTab, text = "Examination",cursor = "hand2",borderwidth = 0)
+    examination.pack(fill =X,padx=10, pady=20)
+
+    contact = Button(menuTab, text = "Contact Us",cursor = "hand2",borderwidth = 0, command = contactUs)
+    contact.pack(fill =X,padx=10, pady=20)
+
+    profile = Button(menuTab, text = "Profile",cursor = "hand2",borderwidth = 0, command= showStudentDetails)
+    profile.pack(fill =X,padx=10, pady=20)
+
+    logout = Button(menuTab, text = "Logout", cursor="hand2",borderwidth = 0)
+    profile.pack(fill= X, padx = 10)
+
+
+    home_frame = Frame(padx = 20,pady = 20,borderwidth = 3, bg = "red")
+    home_frame.pack(fill = X)
+
 
 #--------Variables----------
 
@@ -97,7 +242,12 @@ def student_login_page():
                                                                                                   padx=10, pady=10)
     f_2.pack(anchor="w")
 
-    Button(fbmain, text="Connect", cursor="hand2", bg="#0074D9", fg="white", relief=SUNKEN, font="Arial 12 bold").pack()
+    # Forget function for the student dashboard--------------------------------------------------------------------
+    def studentDashboardPage():
+        f1.forget()
+        fbmain.forget()
+        studentDashboard()
+    Button(fbmain, text="Connect", cursor="hand2", bg="#0074D9", fg="white", relief=SUNKEN, font="Arial 12 bold", command = studentDashboardPage).pack()
 
     fbmain.pack(anchor="w", padx=250, pady=20, fill="x")
 
